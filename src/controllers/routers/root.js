12c3fef22d4a732942cf3234f47509c95b9b5890
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 00:38:07 -0300

    implementa router accounts no root

--- src/controllers/routers/root.js
+++ src/controllers/routers/root.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const accounts = require('./accounts');
+
 const root = express.Router({ mergeParams: true });
+
+root.use('/accounts', accounts);
 
 module.exports = root;
