2569ae886cba99155ea2124a6834d0c00dcc1a8e
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 13:05:08 -0300

    implementa valueLimitVerify no index do services validations

--- src/services/validations/index.js
+++ src/services/validations/index.js
@@ -2,10 +2,12 @@ const cpfVerify = require('./cpfVerify');
 const fullNameVerify = require('./fullNameVerify');
 const passwordVerify = require('./passwordVerify');
 const valueVerify = require('./valueVerify');
+const valueLimitVerify = require('./valueLimitVerify');
 
 module.exports = {
   cpfVerify,
   fullNameVerify,
   passwordVerify,
   valueVerify,
+  valueLimitVerify,
 };
