29dacbf2bd410d9e3bd848362b3eddb3fa4d015d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 09:31:04 -0300

    implementa router deposits no root

--- src/controllers/routers/root.js
+++ src/controllers/routers/root.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const accounts = require('./accounts');
 const login = require('./login');
+const deposits = require('./deposits');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/accounts', accounts);
 root.use('/login', login);
+root.use('/deposits', deposits);
 
 module.exports = root;
