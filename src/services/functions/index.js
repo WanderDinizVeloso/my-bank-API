c8bdf34f948f84d625c6e80ef1ce02415122bfae
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 13:28:40 -0300

    implementa protectCpf no index do services functions

--- src/services/functions/index.js
+++ src/services/functions/index.js
@@ -1,7 +1,9 @@
 const stringInNumber = require('./stringInNumber');
 const setToTwoDecimalPlaces = require('./setToTwoDecimalPlaces');
+const protectCpf = require('./protectCpf');
 
 module.exports = {
   stringInNumber,
   setToTwoDecimalPlaces,
+  protectCpf,
 };
