5f24be39f0d2eaf91a7bd41a8f4070aa9c9dbe2e
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 12:32:43 -0300

    cria services validations passwordVerify

---
+++ src/services/validations/passwordVerify.js
@@ -0,0 +1,15 @@
+module.exports = (password) => {
+  const upperCaseRegex = /[A-Z]+/.test(password);
+  const numberRegex = /[0-9]+/.test(password);
+  const specialCharacterRegex = /[!$#%_]+/.test(password);
+
+  const characterArray = [upperCaseRegex, numberRegex, specialCharacterRegex];
+
+  const characterNotValid = characterArray.some((character) => !character);
+
+  if (characterNotValid) {
+    return null;
+  }
+
+  return password;
+};
