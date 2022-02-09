eefca025a03370f07945e6c7cf8961eeb9a66002
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 00:56:59 -0300

    cria CPF_OR_PASSWORD no services strings

--- src/services/strings/index.js
+++ src/services/strings/index.js
@@ -2,10 +2,12 @@ const TOKEN = 'token';
 const ACCOUNT = 'account';
 const ACCOUNTS = 'accounts';
 const CPF = 'cpf';
+const CPF_OR_PASSWORD = 'cpf or password';
 
 module.exports = {
   TOKEN,
   ACCOUNT,
   ACCOUNTS,
   CPF,
+  CPF_OR_PASSWORD,
 };
