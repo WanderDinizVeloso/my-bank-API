e8a5f98adbf74db95e08e6ae69db0dc55d0b7a86
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 10:07:03 -0300

    implementa router transfers no root

--- src/controllers/routers/root.js
+++ src/controllers/routers/root.js
@@ -3,11 +3,13 @@ const express = require('express');
 const accounts = require('./accounts');
 const login = require('./login');
 const deposits = require('./deposits');
+const transfers = require('./transfers');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/accounts', accounts);
 root.use('/login', login);
 root.use('/deposits', deposits);
+root.use('/transfers', transfers);
 
 module.exports = root;
