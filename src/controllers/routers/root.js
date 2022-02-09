52c6f689659afa308a5851f61e96ea1710c95552
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 01:01:47 -0300

    implementa router login no root

--- src/controllers/routers/root.js
+++ src/controllers/routers/root.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const accounts = require('./accounts');
+const login = require('./login');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/accounts', accounts);
+root.use('/login', login);
 
 module.exports = root;
