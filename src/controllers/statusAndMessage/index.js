dc465be4dfdccce084a8342a0229ef400b8776eb
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 18:07:55 -0300

    cria function invalidAttribute no controllers statusAndMessage

--- src/controllers/statusAndMessage/index.js
+++ src/controllers/statusAndMessage/index.js
@@ -21,10 +21,19 @@ const registered = (param) => ({
   message: `'${param}' is already.`,
 });
 
+const invalidAttribute = (param, type, length) => ({
+  status: BAD_REQUEST,
+  message: `The ${param} must meet the following requirements:`
+  + '- Is required;'
+  + `- Must be a ${type};`
+  + `- Must contain at least ${length} characters.`,
+});
+
 module.exports = {
   invalid,
   notFound,
   internalError,
   createdSuccessfully,
   registered,
+  invalidAttribute,
 };
