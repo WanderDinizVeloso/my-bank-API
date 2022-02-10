0a6002aaaaa5cefad01e3c62d874bc88763d9c10
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 23:05:27 -0300

    altera implementacao do cpfVerify no middleware validateCpf

--- src/controllers/middlewares/validations/validateCpf.js
+++ src/controllers/middlewares/validations/validateCpf.js
@@ -1,5 +1,6 @@
 const { cpfVerify } = require('../../../services/validations');
 const { invalidCpf } = require('../../statusAndMessage');
+const { CPF } = require('../../../services/strings');
 
 module.exports = async (req, _res, next) => {
   const { cpf } = req.body;
@@ -7,7 +8,7 @@ module.exports = async (req, _res, next) => {
   const verifiedCpf = cpfVerify(cpf);
 
   if (!verifiedCpf) {
-    return next(invalidCpf());
+    return next(invalidCpf(CPF));
   }
 
   return next();
