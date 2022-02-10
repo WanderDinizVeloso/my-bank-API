62bf3019d7391e0c7d37903fcf01ab16bf2f52d0
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-10 16:16:28 -0300

    cria teste para o services functions ProtectAccountData

---
+++ __tests__/unit/services/functions/protectAccountData.test.js
@@ -0,0 +1,18 @@
+const { protectAccountData } = require('../../../../src/services/functions');
+
+describe('protectAccountData tests', () => {
+  it('should return "{ fullName: "FullNameTest", cpf: "***.456.789-**" }" when data is "{ fullName: "FullNameTest", cpf: "123.456.789-00" }"',
+    () => {
+      const data = {
+        fullName: 'FullNameTest',
+        cpf: '123.456.789-00',
+      };
+
+      const response = ({
+        fullName: 'FullNameTest',
+        cpf: '***.456.789-**',
+      })
+
+      expect(protectAccountData(data)).toEqual(response);
+    });
+});
