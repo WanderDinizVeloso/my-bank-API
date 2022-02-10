82b7f670fc7c9650dd87c8e36023cab2dca49d0f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 23:09:22 -0300

    implementa vaidateCpf, validateFullName e validatePassword no index do controllers middlewares

--- src/controllers/middlewares/index.js
+++ src/controllers/middlewares/index.js
@@ -1,9 +1,15 @@
 const auth = require('./auth');
 const error = require('./error');
 const wrapper = require('./wrapper');
+const validateCpf = require('./validations/validateCpf');
+const validateFullName = require('./validations/validateFullName');
+const validatePassword = require('./validations/validatePassword');
 
 module.exports = {
   auth,
   error,
   wrapper,
+  validateCpf,
+  validateFullName,
+  validatePassword,
 };
