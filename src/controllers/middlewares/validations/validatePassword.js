795d84ff64416c4ea940805bfb57d06bd5bb3f98
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 23:00:05 -0300

    cria middleware validatePassword

---
+++ src/controllers/middlewares/validations/validatePassword.js
@@ -0,0 +1,16 @@
+const { passwordVerify } = require('../../../services/validations');
+const { invalidPassword } = require('../../statusAndMessage');
+const { STRING } = require('../../../services/strings');
+const { PASSWORD_LENGTH } = require('../../../services/magicNumbers');
+
+module.exports = async (req, _res, next) => {
+  const { password } = req.body;
+
+  const verifiedPassword = passwordVerify(password);
+
+  if (!verifiedPassword) {
+    return next(invalidPassword(STRING, PASSWORD_LENGTH));
+  }
+
+  return next();
+};
