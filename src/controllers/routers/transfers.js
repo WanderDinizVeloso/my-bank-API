8f9aa15a2c712febd04f6074c70ac36f7dc4184f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-10 00:00:27 -0300

    implementa validateDestinationCpf e validateValue no router transfers

--- src/controllers/routers/transfers.js
+++ src/controllers/routers/transfers.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
-const { wrapper, auth } = require('../middlewares');
+const { wrapper, auth, validateDestinationCpf, validateValue } = require('../middlewares');
 const { create } = require('../documents/transfers');
 
 const router = express.Router({ mergeParams: true });
 
 router.post('/', wrapper([
   auth,
+  validateDestinationCpf,
+  validateValue,
   create,
 ]));
 
