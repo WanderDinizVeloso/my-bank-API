84273e19942368f92b2df052496d89ca5eeada2e
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-09 23:43:46 -0300

    retira valueLimitVerify do index do services validations

--- src/services/validations/index.js
+++ src/services/validations/index.js
@@ -2,12 +2,10 @@ const cpfVerify = require('./cpfVerify');
 const fullNameVerify = require('./fullNameVerify');
 const passwordVerify = require('./passwordVerify');
 const valueVerify = require('./valueVerify');
-const valueLimitVerify = require('./valueLimitVerify');
 
 module.exports = {
   cpfVerify,
   fullNameVerify,
   passwordVerify,
   valueVerify,
-  valueLimitVerify,
 };
