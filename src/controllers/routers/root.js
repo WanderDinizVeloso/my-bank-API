0cd8b633ce185520d39f15de10c3553a067fc80a
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-02-11 16:32:28 -0300

    implementa router home no root

--- src/controllers/routers/root.js
+++ src/controllers/routers/root.js
@@ -4,6 +4,7 @@ const accounts = require('./accounts');
 const login = require('./login');
 const deposits = require('./deposits');
 const transfers = require('./transfers');
+const home = require('./home');
 
 const root = express.Router({ mergeParams: true });
 
@@ -11,5 +12,6 @@ root.use('/accounts', accounts);
 root.use('/login', login);
 root.use('/deposits', deposits);
 root.use('/transfers', transfers);
+root.use('/', home);
 
 module.exports = root;
